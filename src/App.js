db7cff388ce961ce12ab078408c295091804755b
Azher Saeed <chazher1997@gmail.com>
2020-06-28 19:11:13 +0500

    child component is render and css boilerplate file is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from '../components/Header'
 
 import './App.css';
 
 function App() {
   return (
     <div>
-     <h1>Action run or not</h1>
+     <Header/>
     </div>
   );
 }
